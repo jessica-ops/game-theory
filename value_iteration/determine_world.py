3c19907136d4abed29250ac9d5f71f34cbfc3732
jessicaops <jessica.opsahl.ong@gmail.com>
2022-08-08 16:35:08 -0500

    agent play via boltzmann equation, determine agent identity

--- value_iteration/determine_world.py
+++ value_iteration/determine_world.py
@@ -1,9 +1,13 @@
+from argparse import Action
 import io
 import numpy as np
 import math
 import random
 import sys
+import matplotlib.pyplot as plt
+
 sys.path.append('..')
+from strategies.strategy import *
 from strategies.action_probability import ActionProbability
 
 PAYOFF = [[(10.0, 10.0), (0.0, 20.0)], [(20.0, 0.0), (5.0, 5.0)]]
@@ -55,34 +59,40 @@ def load_compressed_table(filename):
     
     return table
 
-def io_play(Q, next_act_dist, epsilon, rounds, choice_param, names, distributions):
-    prior = np.ones(shape = (len(distributions), )) / 0.5
-    posterior = np.zeros(shape = (len(distributions), ))
+def act_prob_from_Q(Q, next_act_dist, opp_history, agent_history, opp_history_str, agent_history_str, epsilon, temperature, rounds):
+    opp_next_act_dist = next_act_dist(opp_history, agent_history, epsilon)
+    values = [[Q[(opp_history_str + "0", agent_history_str)][0], 
+    Q[(opp_history_str + "1", agent_history_str)][0]], 
+    [Q[(opp_history_str + "0", agent_history_str)][1], 
+    Q[(opp_history_str + "1", agent_history_str)][1]]]
+
+    if round != rounds - 1:
+        values[0][0] *= opp_next_act_dist[0]
+        values[1][0] *= opp_next_act_dist[0]
+        values[0][1] *= opp_next_act_dist[1]
+        values[1][1] *= opp_next_act_dist[1]
+    
+    action_value = np.array([sum(values[0]), sum(values[1])])
+    norm = sum(np.exp(action_value / temperature))
+    act_probs = np.exp(action_value / temperature) / norm
+
+    return act_probs    
+
+def io_play(Q, next_act_dist, epsilon, rounds, temperature, names, Qs, next_act_dists):
     while True:
+        prior = np.ones(shape = (len(names), )) / 0.5
+        likelihood = np.zeros(shape = (len(names), ))
         io_history = []
         agent_history = []
         io_history_str = ""
         agent_history_str = "None"
         for round in range(rounds):
-            opp_next_act_dist = next_act_dist(io_history, agent_history, epsilon)
-            values = [[Q[(io_history_str + "0", agent_history_str)][0], 
-            Q[(io_history_str + "1", agent_history_str)][0]], 
-            [Q[(io_history_str + "0", agent_history_str)][1], 
-            Q[(io_history_str + "1", agent_history_str)][1]]]
-            # print("Q values:", values)
-            if round != rounds - 1:
-                values[0][0] *= opp_next_act_dist[0]
-                values[1][0] *= opp_next_act_dist[0]
-                values[0][1] *= opp_next_act_dist[1]
-                values[1][1] *= opp_next_act_dist[1]
-            # print("expected q values:", values)
-            action_value = [sum(values[0]), sum(values[1])]
-            # print("action values:", action_value)
-            my_action = np.argmax(action_value)
-            threshold = math.pow(choice_param, abs(action_value[0] - action_value[1])) / 2.0
+            act_probs = act_prob_from_Q(Q, next_act_dist, io_history, agent_history, io_history_str, agent_history_str, epsilon, temperature, rounds)
             prob = random.random()
-            if prob <= threshold:
-                my_action = (my_action + 1) % 2
+            if prob <= act_probs[0]:
+                my_action = 0
+            else:
+                my_action = 1
             
             io_action = int(input("Enter an action: "))
             my_reward, io_reward = PAYOFF[my_action][io_action]
@@ -90,17 +100,25 @@ def io_play(Q, next_act_dist, epsilon, rounds, choice_param, names, distribution
             print("my action:", my_action, "strat action:", io_action)
             print("my reward:", my_reward, "strat reward:", io_reward)
             print("")
-            print("P(agent action =" + str(my_action) + " | "+ io_history_str + ", strat = _____)")
+            #print("P(agent action =" + str(my_action) + " | "+ io_history_str + ", strat = _____)")
+
             for i in range(len(names)):
-                if io_history_str in distributions[i]:
-                    if my_action in distributions[i][io_history_str]:
-                        # print(distributions[i][io_history_str])
-                        print(names[i] + " =", distributions[i][io_history_str][my_action])
-                    else: 
-                        print(names[i] + " = 0.0")
-                else:
-                    print(names[i] + " has not been observed with (" + io_history_str + ")")
+                print(names[i][0])
+                act_lh = act_prob_from_Q(Qs[i], next_act_dists[i], io_history, agent_history, io_history_str, agent_history_str, names[i][1], temperature, rounds)
+                print(act_lh)
+                likelihood[i] = act_lh[my_action]
+            
+            marginal = 0
+            for i in range(len(likelihood)):
+                marginal += likelihood[i] * prior[i]
+            
+            posterior = np.multiply(likelihood, prior) / marginal
+
+            for i in range(len(posterior)):
+                print(names[i][0] + " = " + str(posterior[i]))
             print("")
+            prior = posterior
+
             agent_history.append(my_action)
             io_history.append(io_action)
             io_history_str = io_history_str + str(io_action)
@@ -111,16 +129,109 @@ def io_play(Q, next_act_dist, epsilon, rounds, choice_param, names, distribution
         
         print("\n END OF THIS GAME \n")
 
-def main():
-    Q = load_table("policies/epsilon_0.2/Tit for Tat.txt")
-    #print(Q)
-    names = ["Cooperator 0.1", "Tit for Tat 0.2", "Tit for Tat 0.4", "Tit for Two Tats 0.1"]
-    dist_names = ["epsilon_0.1/Cooperator", "epsilon_0.2/Tit for Tat", "epsilon_0.4/Tit for Tat", "epsilon_0.1/Tit for Two Tats"]
-    dists = []
-    for dist_name in dist_names:
-        dists.append(load_compressed_table("compressed_obvs/" + dist_name + ".txt"))
+def agent_vs_strat(Q, epsilon, temperature, opponent, next_act_dist, rounds):
+    strat_history = []
+    agent_history = []
+    strat_history_str = ""
+    agent_history_str = "None"
+    for round in range(rounds):
+        act_probs = act_prob_from_Q(Q, next_act_dist, strat_history, agent_history, strat_history_str, agent_history_str, epsilon, temperature, rounds)
+        prob = random.random()
+        if prob <= act_probs[0]:
+            my_action = 0
+        else:
+            my_action = 1
+        
+        strat_act = opponent.choose(strat_history, agent_history, round)
+        agent_history.append(my_action)
+        strat_history.append(strat_act)
+        strat_history_str = strat_history_str + str(strat_act)
+        if agent_history_str == "None":
+            agent_history_str = str(my_action)
+        else:
+            agent_history_str = agent_history_str + str(my_action)
     
-    io_play(Q, ActionProbability.tit_for_tat, 0.2, 10, 0.85, names, dists)
+    return agent_history, strat_history, agent_history_str, strat_history_str
+
+def analyze_game(agent_history, strat_history, agent_history_str, strat_history_str, names, potential_Qs, next_act_dists, temperature):
+    likelihood = np.ones(shape = (len(names), ))
+    # print("agent history:", agent_history)
+    # print("strat history:", strat_history)
+    for round in range(len(agent_history)):
+        for i in range(len(names)):
+            # print(names[i])
+            if round == 0:
+                act_lh = act_prob_from_Q(potential_Qs[i], next_act_dists[i], strat_history[:round], agent_history[:round], strat_history_str[:round], "None", names[i][1], temperature, len(agent_history))
+            else:
+                act_lh = act_prob_from_Q(potential_Qs[i], next_act_dists[i], strat_history[:round], agent_history[:round], strat_history_str[:round], agent_history_str[:round], names[i][1], temperature, len(agent_history))
+            # print(act_lh, ",", agent_history[round])
+            likelihood[i] *= act_lh[agent_history[round]]
+    
+    return likelihood
+
+def main():
+    #print(Q)
+    temperature = 2
+    rounds = 10
+    games = 1000
+    # names = [("Cooperator", 0.2), ("Defector", 0.2), ("Tit for Tat", 0.2), ("Tit for Two Tats", 0.2), ("Soft Majority", 0.2)]
+    names = [("Cooperator", 0.2), ("Defector", 0.2), ("Tit for Tat", 0.2), ("Grim Trigger", 0.2),("Tit for Two Tats", 0.2),
+    ("Two Tits for Tat", 0.2), ("Gradual", 0.2), ("Soft Majority", 0.2), ("Hard Majority", 0.2), ("Remorseful Prober", 0.2), 
+    ("Soft Grudger", 0.2), ("Prober", 0.2)]
+    labels = possibilities = ["Cooperator", "Defector", "Tit for Tat", "Grim Trigger", "Tit for Two Tats", 
+    "Two Tits for Tat", "Gradual", "Soft Majority", "Hard Majority", "Remoreseful Prober", "Soft Grudger", 
+    "Prober"]
+    # opponents = [Cooperater(0.2), Defector(0.2), TitForTat(0.2), TitForTwoTats(0.2), SoftMajority(0.2)]
+    opponents = [Cooperater(0.2), Defector(0.2), TitForTat(0.2), GrimTrigger(0.2), TitForTwoTats(0.2), TwoTitsForTats(0.2), Gradual(0.2), 
+    SoftMajority(0.2), HardMajority(0.2), RemorsefulProber(0.2), SoftGrudger(0.2), Prober(0.2)]
+    # next_act_dists = [ActionProbability.cooperator, ActionProbability.defector, ActionProbability.tit_for_tat, ActionProbability.tit_for_two_tats, ActionProbability.soft_majority]
+    next_act_dists = [ActionProbability.cooperator, ActionProbability.defector,
+    ActionProbability.tit_for_tat, ActionProbability.grim_trigger, ActionProbability.tit_for_two_tats, 
+    ActionProbability.two_tits_for_tat, ActionProbability.gradual, ActionProbability.soft_majority, ActionProbability.hard_majority,
+    ActionProbability.remorseful_prober, ActionProbability.soft_grudger, 
+    ActionProbability.prober]
+    Qs = []
+    for i in range(len(names)):
+        folder = "policies/epsilon_" + str(names[i][1]) + "/"
+        Qs.append(load_table(folder + names[i][0] + ".txt"))
+
+    posteriors = [[] for i in range(len(names))]
+    for g in range(games):
+        for i in range(len(names)):
+            prior = np.ones(len(names)) / len(names)
+            likelihood = np.ones(len(names))
+            Q = Qs[i]
+            for j in range(len(names)):
+                #print("Agent trained against " + names[i][0] + "(" + str(names[i][1]) + ") -- vs -- " + names[j][0] + "(" + str(names[j][1]) + ")")
+                agent_history, strat_history, agent_history_str, strat_history_str = agent_vs_strat(Q, names[i][1], temperature, opponents[j], next_act_dists[i], rounds)
+                new_likelihood = analyze_game(agent_history, strat_history, agent_history_str, strat_history_str, names, Qs, next_act_dists, temperature)
+                likelihood = np.multiply(likelihood, new_likelihood)
+            posterior = np.multiply(prior, likelihood) / np.sum(np.multiply(prior, likelihood))
+            posteriors[i].append(posterior)
+    
+    x = np.linspace(1, len(names) * 3, len(names))
+    cmap = plt.cm.get_cmap('tab20', len(names))
+    # plt.figure(figsize=(20,10))
+    # #print(posteriors)
+    # plt.xticks(x, labels)
+    # for i in range(len(names)):
+    #     plt.plot(x, np.average(posteriors[i], axis=0), color = cmap(i), linewidth = 5.0)
+    # plt.savefig("graphs/total")
+    # plt.close()
+
+    # plt.figure(figsize=(20,10))
+    for i in range(len(names)):
+        plt.plot(x, np.average(posteriors[i], axis=0), color = cmap(i), linewidth = 5.0)
+        plt.ylim([0.0, 1.0])
+        plt.savefig("graphs/" + names[i][0])
+        plt.close()
+    
+    # plt.figure(figsize=(20,10))
+    # for i in range(len(names)):
+    #     if names[i][0] == "Cooperator" or names[i][0] == "Defector" or names[i][0] == "Tit for Tat" or names[i][0] == "Tit for Two Tats":
+    #         plt.plot(x, np.average(posteriors[i], axis=0), color = cmap(i), linewidth = 5.0)
+    # plt.savefig("graphs/four dists")
+    # plt.close()
 
 if __name__ == "__main__":
     main()
